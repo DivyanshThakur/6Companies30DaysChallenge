cf2ae6bb148b3f10b82858ff8804be1646ebf900
Divyansh Singh Thakur <divyansh2140@gmail.com>
2022-01-05 16:41:33 +0530

    Goldman Sachs 15 Questions Completed

--- 1. Goldman Sachs/Q8.cpp
+++ 1. Goldman Sachs/Q8.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 
 using namespace std;
+
+class Solution
+{
+    int mod = 1e9 + 7;
+
+public:
+    int CountWays(string str)
+    {
+        if (str[0] == '0')
+            return 0;
+
+        int n = str.size();
+
+        if (n <= 1)
+            return 1;
+
+        int a = 1, b = 1, c;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (str[i] == '0' and str[i - 1] > '2')
+                return 0;
+        }
+
+        for (int i = 2; i <= n; i++)
+        {
+            c = 0;
+            if (str[i - 1] > '0')
+                c = b;
+            if (str[i - 2] == '1' or (str[i - 2] == '2' and str[i - 1] < '7'))
+            {
+                c = (c + a) % mod;
+            }
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+};
