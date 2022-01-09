785cee061a7ddf11751c88d80a8c8a0416e9d090
Divyansh Singh Thakur <divyansh2140@gmail.com>
2022-01-09 17:05:25 +0530

    q 9 completed

---
+++ 2. Amazon/Q9.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int isValid(vector<vector<int>> mat)
+    {
+        vector<set<int>> row(9), col(9), grid(9);
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                int val = mat[i][j];
+
+                if (val == 0)
+                    continue;
+
+                if (row[i].count(val) or col[j].count(val) or grid[3 * (i / 3) + j / 3].count(val))
+                    return false;
+                row[i].insert(val);
+                col[j].insert(val);
+                grid[3 * (i / 3) + j / 3].insert(val);
+            }
+        }
+
+        return true;
+    }
+};
